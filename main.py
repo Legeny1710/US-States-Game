fcb3bf84548968f105b35e3357d6aa108e2d4bd8
Yusuf Kula <yvk2045@gmail.com>
2023-08-04 19:36:15 +0100

    Added a new feature: when user finishes, states that they couldnt guess appened to new csv file

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import turtle
+
+import pandas
 import pandas as pd
 
 img = "blank_states_img.gif"
@@ -11,7 +13,7 @@ screen.addshape(img)
 turtle.shape(img)
 
 state_guessed = 0
-
+states_guessed_list = []
 
 
 df = pd.read_csv("50_states.csv")
@@ -19,15 +21,27 @@ df = pd.read_csv("50_states.csv")
 
 
 while state_guessed != 50:
-    answer_state = screen.textinput(title=f"{state_guessed}/50 states guessed", prompt="What's another state's name?: ").capitalize()
-    for state in df["state"]:
-        if state == answer_state:
-            state_turtle = turtle.Turtle()
-            state_turtle.hideturtle()
-            state_turtle.penup()
-            state_turtle.goto(int(df[df["state"] == state].x), int(df[df["state"] == state].y))
-            state_turtle.write(f"{state}", align="center", font=("Courier", 10, "normal"))
-            state_guessed += 1
+    answer_state = screen.textinput(title=f"{state_guessed}/50 states guessed", prompt="What's another state's name?: ").title()
+    if answer_state == "Exit":
+        missing_states = []
+        for i in df["state"]:
+            if i not in states_guessed_list:
+                missing_states.append(i)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn")
+        exit()
+    else:
+        for state in df["state"]:
+            if state == answer_state:
+                state_turtle = turtle.Turtle()
+                state_turtle.hideturtle()
+                state_turtle.penup()
+                state_turtle.goto(int(df[df["state"] == state].x), int(df[df["state"] == state].y))
+                state_turtle.write(f"{state}", align="center", font=("Courier", 10, "normal"))
+                state_guessed += 1
+                states_guessed_list.append(state)
+
+
 
 
 
